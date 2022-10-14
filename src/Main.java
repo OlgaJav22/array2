35e029544b72af6a33304c576e852edd4b913c19
OlgaJav22 <olga.ko4erga@yandex.ru>
2022-10-15 01:01:25 +0300

    уже лучше)

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,38 @@
+
 public class Main {
+
     public static void main(String[] args) {
 
+        //Задача 1
 
+    int sum = 0;
+    int [] arr = new int [] {25, 40, 35, 50, 65, 75, 80, 198, 205, 89, 13, 17, 15, 10, 5, 12, 24, 18, 20, 27, 48, 125, 148, 235, 267, 254, 198, 147, 1321, 30};
+    for (int i =0; i< arr.length; i++) {
+        sum +=arr[i];
     }
+        System.out.println("Сумма трат за месяц составила: " + sum + " рублей");
 
 
-    int[] arr = generateRandomArray();
+    //Задача 2
+        System.out.println();
 
-    public static int[] generateRandomArray() {
-        java.util.Random random = new java.util.Random();
-        int[] arr = new int[30];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = random.nextInt(100_000) + 100_000;
+    int minSum = 500;
+    int maxSum = 0;
+    for (int j =0; j< arr.length; j++){
+        if (arr[j] >maxSum) {
+            maxSum=arr[j];
         }
-        return arr;
+        if (arr[j] <minSum) {
+            minSum=arr[j];
+        }
     }
+        System.out.println("Минимальная сумма трат за день составила: " +  minSum + " рублей." + " Максимальная сумма трат за день составила: " + maxSum + " рублей.");
 
+    //Задача 3
+        System.out.println();
+
+
+
+    }
 }
+
