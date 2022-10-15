48407e13faf717b43a2a96f754a1f2c73c04a1dd
OlgaJav22 <olga.ko4erga@yandex.ru>
2022-10-15 23:10:12 +0300

    Ура, готово!

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class Main {
 
@@ -6,7 +7,7 @@ public class Main {
         //Задача 1
 
     int sum = 0;
-    int [] arr = new int [] {25, 40, 35, 50, 65, 75, 80, 198, 205, 89, 13, 17, 15, 10, 5, 12, 24, 18, 20, 27, 48, 125, 148, 235, 267, 254, 198, 147, 1321, 30};
+    int [] arr = new int [] {25, 40, 35, 50, 65, 75, 80, 198, 205, 89, 13, 17, 15, 10, 5, 12, 24, 18, 20, 27, 48, 125, 148, 235, 267, 254, 198, 147, 1321, 300};
     for (int i =0; i< arr.length; i++) {
         sum +=arr[i];
     }
@@ -31,8 +32,22 @@ public class Main {
     //Задача 3
         System.out.println();
 
+    int maxIndex= arr.length;
+    double averageSum = (double) sum/maxIndex;
 
+        System.out.printf("Средняя сумма трат за месяц составила  - %.2f" , averageSum, " рублей.");
 
+    //Задача 4
+        System.out.println();
+        System.out.println();
+
+        char [] information = new char [] {'n', 'a', 'v', 'I', ' ','v', 'o', 'n', 'a', 'v', 'I'};
+        for (int p = information.length-1; p>=0;p--){
+
+            System.out.print(information[p]);
+
+        }
     }
 }
+
 
